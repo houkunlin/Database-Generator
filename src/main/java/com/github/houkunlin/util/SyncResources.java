211283728e08ab2fb52b4d0d7c620c0dd4fe2bf5
侯坤林 <houkunlin@aliyun.com>
2020-07-05 02:52:44 +0800

    refactor: 修改插件运行时同步模板文件代码，改为从资源文件中读取需要同步的文件列表

--- src/main/java/com/github/houkunlin/util/SyncResources.java
+++ src/main/java/com/github/houkunlin/util/SyncResources.java
@@ -2,6 +2,7 @@ package com.github.houkunlin.util;
 
 import com.intellij.xml.actions.xmlbeans.FileUtils;
 import lombok.Data;
+import org.apache.commons.lang.StringUtils;
 
 import java.io.File;
 import java.io.IOException;
@@ -35,10 +36,19 @@ public class SyncResources implements Runnable {
             return;
         }
         try {
-            copyResourcesToProject(ContextUtils.getLocalConfigPath(), "", "init.properties", "types.json", "options.json", "settings.json", "developer.json");
-            copyResourcesToProject(ContextUtils.getTemplatesPath(), "templates/",
-                    "backend_Controller.java.ftl", "backend_Entity.java.ftl", "backend_Mapper.xml.ftl",
-                    "backend_MyBatisUtils.java.ftl", "backend_Repository.java.ftl", "backend_Service.java.ftl", "backend_ServiceImpl.java.ftl");
+            copyResourcesToProject(ContextUtils.getLocalConfigPath(), "init.properties", "types.json", "options.json", "settings.json", "developer.json");
+            String syncFiles = IO.readResources("syncFiles.txt");
+            String[] split = syncFiles.split("\n");
+            for (String filePath : split) {
+                if (StringUtils.isBlank(filePath)) {
+                    continue;
+                }
+                InputStream inputStream = IO.getInputStream(filePath);
+                if (inputStream == null) {
+                    continue;
+                }
+                FileUtils.saveStreamContentAsFile(ContextUtils.getTemplatesPath() + File.separator + filePath.replace("templates/", ""), inputStream);
+            }
         } catch (Exception ignore) {
         }
     }
@@ -47,13 +57,12 @@ public class SyncResources implements Runnable {
     /**
      * 复制插件资源到本地项目中
      *
-     * @param resourcesPath 路径
      * @param filenames     文件名称
      * @throws IOException 异常
      */
-    private void copyResourcesToProject(File savePath, String resourcesPath, String... filenames) throws IOException {
+    private void copyResourcesToProject(File savePath, String... filenames) throws IOException {
         for (String filename : filenames) {
-            InputStream inputStream = IO.getInputStream(resourcesPath + filename);
+            InputStream inputStream = IO.getInputStream(filename);
             if (inputStream == null) {
                 continue;
             }
