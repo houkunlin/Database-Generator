aa9542e156ac1139e646281dad346e73d2199116
侯坤林 <houkunlin@aliyun.com>
2020-08-20 13:00:18 +0800

    perf: 优化基础设置页面的输入框内容改变事件赋值代码

--- src/main/java/com/github/houkunlin/ui/win/BaseSetting.java
+++ src/main/java/com/github/houkunlin/ui/win/BaseSetting.java
@@ -32,7 +32,7 @@ import java.util.function.Consumer;
  * @author HouKunLin
  * @date 2020/8/15 0015 16:00
  */
-public class BaseSetting implements IWindows, DocumentListener, com.intellij.openapi.editor.event.DocumentListener, ItemListener {
+public class BaseSetting implements IWindows {
     /**
      * 配置对象：设置信息
      */
@@ -41,18 +41,6 @@ public class BaseSetting implements IWindows, DocumentListener, com.intellij.ope
      * 配置对象：开发者信息
      */
     private final Developer developer;
-    /**
-     * 配置对象：其他参数
-     */
-    private final Options options;
-    /**
-     * 存放 setValue 与 输入框 的关系
-     */
-    private final Map<Consumer<String>, JTextComponent> map1 = Maps.newHashMap();
-    /**
-     * 存放 setValue 与 输入框 的关系
-     */
-    private final Map<Consumer<String>, EditorTextField> map2 = Maps.newHashMap();
     /**
      * 输入框：Entity前缀
      */
@@ -117,67 +105,158 @@ public class BaseSetting implements IWindows, DocumentListener, com.intellij.ope
      * 面板：顶级页面面板
      */
     private JPanel content;
+    /**
+     * 复选：是否覆盖Java文件
+     */
     private JCheckBox overrideJavaCheckBox;
+    /**
+     * 复选：是否覆盖XML文件
+     */
     private JCheckBox overrideXmlCheckBox;
+    /**
+     * 复选：是否覆盖其他文件
+     */
     private JCheckBox overrideOtherCheckBox;
 
     public BaseSetting(Settings settings, Developer developer, Options options) {
         this.settings = settings;
         this.developer = developer;
-        this.options = options;
         initDeveloper();
         initSettings();
         configSelectPackage();
 
-        authorField.getDocument().addDocumentListener(this);
-        emailField.getDocument().addDocumentListener(this);
-        entitySuffixField.getDocument().addDocumentListener(this);
-        daoSuffixField.getDocument().addDocumentListener(this);
-        serviceSuffixField.getDocument().addDocumentListener(this);
-        controllerSuffixField.getDocument().addDocumentListener(this);
-        xmlPackageField.getDocument().addDocumentListener(this);
+        /* 普通输入框的输入事件监听 */
+        class TextFieldDocumentListener implements DocumentListener {
+            /**
+             * 存放 setValue 与 输入框 的关系
+             */
+            private final Map<Consumer<String>, JTextComponent> map = Maps.newHashMap();
 
-        map1.put(developer::setAuthor, authorField);
-        map1.put(developer::setEmail, emailField);
-        map1.put(settings::setEntitySuffix, entitySuffixField);
-        map1.put(settings::setDaoSuffix, daoSuffixField);
-        map1.put(settings::setServiceSuffix, serviceSuffixField);
-        map1.put(settings::setControllerSuffix, controllerSuffixField);
-        map1.put(settings::setXmlPackage, xmlPackageField);
+            public TextFieldDocumentListener() {
+                map.put(developer::setAuthor, authorField);
+                map.put(developer::setEmail, emailField);
+                map.put(settings::setEntitySuffix, entitySuffixField);
+                map.put(settings::setDaoSuffix, daoSuffixField);
+                map.put(settings::setServiceSuffix, serviceSuffixField);
+                map.put(settings::setControllerSuffix, controllerSuffixField);
+                map.put(settings::setXmlPackage, xmlPackageField);
+            }
 
-        entityPackageField.getDocument().addDocumentListener(this);
-        daoPackageField.getDocument().addDocumentListener(this);
-        servicePackageField.getDocument().addDocumentListener(this);
-        controllerPackageField.getDocument().addDocumentListener(this);
+            @Override
+            public void insertUpdate(DocumentEvent e) {
+                documentChanged(e);
+            }
 
-        map2.put(settings::setEntityPackage, entityPackageField);
-        map2.put(settings::setDaoPackage, daoPackageField);
-        map2.put(settings::setServicePackage, servicePackageField);
-        map2.put(settings::setControllerPackage, controllerPackageField);
+            @Override
+            public void removeUpdate(DocumentEvent e) {
+                documentChanged(e);
+            }
 
-        overrideJavaCheckBox.addItemListener(this);
-        overrideXmlCheckBox.addItemListener(this);
-        overrideOtherCheckBox.addItemListener(this);
+            @Override
+            public void changedUpdate(DocumentEvent e) {
+            }
+
+            /**
+             * swing 输入框组件内容更改事件
+             *
+             * @param e 事件
+             */
+            public void documentChanged(DocumentEvent e) {
+                javax.swing.text.Document document = e.getDocument();
+                Set<Map.Entry<Consumer<String>, JTextComponent>> entries = map.entrySet();
+                for (Map.Entry<Consumer<String>, JTextComponent> entry : entries) {
+                    if (TextFieldDocumentUtil.updateSettingValue(document, entry.getValue(), entry.getKey())) {
+                        break;
+                    }
+                }
+            }
+        }
+
+        TextFieldDocumentListener textFieldDocumentListener = new TextFieldDocumentListener();
+        authorField.getDocument().addDocumentListener(textFieldDocumentListener);
+        emailField.getDocument().addDocumentListener(textFieldDocumentListener);
+        entitySuffixField.getDocument().addDocumentListener(textFieldDocumentListener);
+        daoSuffixField.getDocument().addDocumentListener(textFieldDocumentListener);
+        serviceSuffixField.getDocument().addDocumentListener(textFieldDocumentListener);
+        controllerSuffixField.getDocument().addDocumentListener(textFieldDocumentListener);
+        xmlPackageField.getDocument().addDocumentListener(textFieldDocumentListener);
+
+        /* 包名输入框的输入事件监听 */
+        class EditorTextFieldDocumentListener implements com.intellij.openapi.editor.event.DocumentListener {
+            /**
+             * 存放 setValue 与 输入框 的关系
+             */
+            private final Map<Consumer<String>, EditorTextField> map = Maps.newHashMap();
+
+            public EditorTextFieldDocumentListener() {
+                map.put(settings::setEntityPackage, entityPackageField);
+                map.put(settings::setDaoPackage, daoPackageField);
+                map.put(settings::setServicePackage, servicePackageField);
+                map.put(settings::setControllerPackage, controllerPackageField);
+            }
+
+            /**
+             * IDEA 输入框组件内容更改事件
+             *
+             * @param event 事件
+             */
+            @Override
+            public void documentChanged(@NotNull com.intellij.openapi.editor.event.DocumentEvent event) {
+                Document document = event.getDocument();
+                Set<Map.Entry<Consumer<String>, EditorTextField>> entries = map.entrySet();
+                for (Map.Entry<Consumer<String>, EditorTextField> entry : entries) {
+                    if (TextFieldDocumentUtil.updateSettingValue(document, entry.getValue(), entry.getKey())) {
+                        break;
+                    }
+                }
+            }
+        }
+
+        EditorTextFieldDocumentListener editorTextFieldDocumentListener = new EditorTextFieldDocumentListener();
+        entityPackageField.getDocument().addDocumentListener(editorTextFieldDocumentListener);
+        daoPackageField.getDocument().addDocumentListener(editorTextFieldDocumentListener);
+        servicePackageField.getDocument().addDocumentListener(editorTextFieldDocumentListener);
+        controllerPackageField.getDocument().addDocumentListener(editorTextFieldDocumentListener);
+
+        ItemListener checkBoxItemListener = new ItemListener() {
+            /**
+             * 复选框勾选事件监听
+             * @param e 事件
+             */
+            @Override
+            public void itemStateChanged(ItemEvent e) {
+                Object item = e.getItem();
+                if (overrideJavaCheckBox == item) {
+                    options.setOverrideJava(overrideJavaCheckBox.isSelected());
+                } else if (overrideXmlCheckBox == item) {
+                    options.setOverrideXml(overrideXmlCheckBox.isSelected());
+                } else if (overrideOtherCheckBox == item) {
+                    options.setOverrideOther(overrideOtherCheckBox.isSelected());
+                }
+            }
+        };
+        overrideJavaCheckBox.addItemListener(checkBoxItemListener);
+        overrideXmlCheckBox.addItemListener(checkBoxItemListener);
+        overrideOtherCheckBox.addItemListener(checkBoxItemListener);
     }
 
     private void createUIComponents() {
         Project project = ContextUtils.getProject();
-        entityPackageField = createEditorTextField(project, "");
-        daoPackageField = createEditorTextField(project, "");
-        servicePackageField = createEditorTextField(project, "");
-        controllerPackageField = createEditorTextField(project, "");
+        entityPackageField = createEditorTextField(project);
+        daoPackageField = createEditorTextField(project);
+        servicePackageField = createEditorTextField(project);
+        controllerPackageField = createEditorTextField(project);
     }
 
     /**
      * 创建一个可以自动补全包名的输入框
      *
      * @param project 项目
-     * @param text    默认文本
      * @return IDEA 输入框组件
      */
-    private EditorTextField createEditorTextField(Project project, String text) {
+    private EditorTextField createEditorTextField(Project project) {
         // https://jetbrains.org/intellij/sdk/docs/user_interface_components/editor_components.html
-        JavaCodeFragment code = JavaCodeFragmentFactory.getInstance(project).createReferenceCodeFragment(text, null, true, false);
+        JavaCodeFragment code = JavaCodeFragmentFactory.getInstance(project).createReferenceCodeFragment("", null, true, false);
         Document document = PsiDocumentManager.getInstance(project).getDocument(code);
         JavaFileType fileType = JavaFileType.INSTANCE;
         return new EditorTextField(document, project, fileType);
@@ -245,63 +324,5 @@ public class BaseSetting implements IWindows, DocumentListener, com.intellij.ope
     @Override
     public JPanel getContent() {
         return content;
-    }
-
-    @Override
-    public void insertUpdate(DocumentEvent e) {
-        documentChanged(e);
-    }
-
-    @Override
-    public void removeUpdate(DocumentEvent e) {
-        documentChanged(e);
-    }
-
-    @Override
-    public void changedUpdate(DocumentEvent e) {
-
-    }
-
-    /**
-     * IDEA 输入框组件内容更改事件
-     *
-     * @param event 事件
-     */
-    @Override
-    public void documentChanged(@NotNull com.intellij.openapi.editor.event.DocumentEvent event) {
-        Document document = event.getDocument();
-        Set<Map.Entry<Consumer<String>, EditorTextField>> entries = map2.entrySet();
-        for (Map.Entry<Consumer<String>, EditorTextField> entry : entries) {
-            if (TextFieldDocumentUtil.updateSettingValue(document, entry.getValue(), entry.getKey())) {
-                break;
-            }
-        }
-    }
-
-    @Override
-    public void itemStateChanged(ItemEvent e) {
-        Object item = e.getItem();
-        if (overrideJavaCheckBox == item) {
-            options.setOverrideJava(overrideJavaCheckBox.isSelected());
-        } else if (overrideXmlCheckBox == item) {
-            options.setOverrideXml(overrideXmlCheckBox.isSelected());
-        } else if (overrideOtherCheckBox == item) {
-            options.setOverrideOther(overrideOtherCheckBox.isSelected());
-        }
-    }
-
-    /**
-     * swing 输入框组件内容更改事件
-     *
-     * @param e 事件
-     */
-    public void documentChanged(DocumentEvent e) {
-        javax.swing.text.Document document = e.getDocument();
-        Set<Map.Entry<Consumer<String>, JTextComponent>> entries = map1.entrySet();
-        for (Map.Entry<Consumer<String>, JTextComponent> entry : entries) {
-            if (TextFieldDocumentUtil.updateSettingValue(document, entry.getValue(), entry.getKey())) {
-                break;
-            }
-        }
     }
 }
